c68723b9aa83f850920d6cfdf7dcaf677c1bd0f2
Onur Şahin <onur@carbonhealth.com>
2023-11-20 18:10:32 +0100

    refactor

--- bin/main.ml
+++ bin/main.ml
@@ -1,9 +1,11 @@
 open Core
-open Omd
+
+let generate_html_from_markdown ~input_file_path ~output_file_path =
+  let file_str = In_channel.read_all input_file_path in
+  let markdown = Omd.of_string file_str in
+  Out_channel.write_all output_file_path ~data:(Omd.to_html markdown)
 
 let () = 
   let current_dir = Core.Sys.getenv_exn "PROJECT_ROOT"  in
   let index_path = Printf.sprintf "%s/content/pages/index.md" current_dir in
-  let index_file_str = In_channel.read_all index_path in
-  let markdown = of_string index_file_str in
-  Out_channel.write_all "index.html" ~data:(Omd.to_html markdown)
+  generate_html_from_markdown ~input_file_path:index_path ~output_file_path:"index.html"
