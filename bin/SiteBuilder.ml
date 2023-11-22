ac02e648942796f0eb488f2cab086f06f0ad1c96
Onur Şahin <onur@carbonhealth.com>
2023-11-22 21:18:04 +0100

    refactor: use Core everywhere

--- bin/SiteBuilder.ml
+++ bin/SiteBuilder.ml
@@ -1,10 +1,16 @@
+open Core
 
 let sprintf = Printf.sprintf
 
 let create_directory ~out_dir ~index_page =
-  if (not (Sys.file_exists out_dir)) || (not (Sys.is_directory out_dir)); then
+  match (Sys_unix.file_exists out_dir, Sys_unix.is_directory out_dir) with
+  | (`No, _) -> 
     print_endline (sprintf "Out directory %s does not exit. Creating..." out_dir);
     Core_unix.mkdir_p ~perm:0o777 out_dir;
+  | (_, `No) ->
+    print_endline (sprintf "Out directory %s already exists and it's not a folder." out_dir);
+    exit 1
+  | _ -> ();
     
   Soup.write_file (sprintf "%s/index.html" out_dir) (Soup.to_string index_page);
   prerr_endline (sprintf "Site generated at: '%s'" out_dir)
