8622c11875461c546c6d08eeeacf99e416c16b92
Onur Şahin <onur@carbonhealth.com>
2023-11-22 21:41:48 +0100

    refactor

--- bin/SiteGenerator.ml
+++ bin/SiteGenerator.ml
@@ -4,32 +4,30 @@ let ( $ ) = Soup.( $ )
 
 open Site
 
-let generate_html_from_markdown ~input_file_path =
-  let file_str = In_channel.read_all input_file_path in
-  let markdown = Omd.of_string file_str in
+let generate_html_from_markdown ~markdown_str =
+  let markdown = Omd.of_string markdown_str in
   Omd.to_html markdown
 
 let instantiate_template_for_index_page (template : Site.page)
     (content : Site.page) =
   Soup.replace (template $ "#page-content") content
 
-let index_page_template_path =
-  Filename.concat Environment.project_root
-    (Filename.of_parts [ "content"; "templates"; "template.html" ])
-
 let generate_index_page () =
-  let index_page_path = Filename.of_parts [ "content"; "pages"; "index.md" ] in
-  let index_page_abs_path =
-    Filename.concat Environment.project_root index_page_path
+  let index_content_path =
+    Filename.concat Environment.project_root
+      (Filename.of_parts [ "content"; "pages"; "index.md" ])
   in
-  let index_page_content =
-    generate_html_from_markdown ~input_file_path:index_page_abs_path
-    |> Soup.parse
+  let index_content_page =
+    generate_html_from_markdown ~markdown_str:(index_content_path |> In_channel.read_all) |> Soup.parse
+  in
+  let index_page_path =
+    Filename.concat Environment.project_root
+      (Filename.of_parts [ "content"; "templates"; "template.html" ])
   in
   let index_page =
-    index_page_template_path |> Core.In_channel.read_all |> Soup.parse
+    index_page_path |> Core.In_channel.read_all |> Soup.parse
   in
-  instantiate_template_for_index_page index_page index_page_content;
+  instantiate_template_for_index_page index_page index_content_page;
   index_page
 
 let generate () = { index_page = generate_index_page () }
