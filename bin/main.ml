07080ceb9477be0fbae1f50619f57f28113ca5f5
Onur Şahin <onur@carbonhealth.com>
2023-11-22 13:59:20 +0100

    refactor: Organize

--- bin/main.ml
+++ bin/main.ml
@@ -1,23 +1,34 @@
+let sprintf = Printf.sprintf;;
+
+let ($) = Soup.($);;
 
 
 let generate_html_from_markdown ~input_file_path =
-  let file_str = Core.In_channel.read_all input_file_path in
+  let file_str = Soup.read_file input_file_path in
   let markdown = Omd.of_string file_str in
   Omd.to_html markdown
 
-let build_site ~out_dir ~(index_html : Soup.soup Soup.node) = 
+let generate_index_page current_dir =
+  let index_path = sprintf "%s/content/pages/index.md" current_dir in
+  let index_html = (generate_html_from_markdown ~input_file_path:index_path) |> Soup.parse in
+  let template_html = (sprintf "%s/content/templates/template.html" current_dir) 
+    |> Soup.read_file
+    |> Soup.parse in
+  Soup.replace (template_html $ "#page-content") index_html;
+  template_html
+
+(* Generate the static site directory *)
+let build_directory ~out_dir ~(index_page : Soup.soup Soup.node) = 
   if (not (Sys.file_exists out_dir)) || (not (Sys.is_directory out_dir)); then
-    print_endline (Printf.sprintf "Out directory %s does not exit. Creating..." out_dir);
+    print_endline (sprintf "Out directory %s does not exit. Creating..." out_dir);
     Core_unix.mkdir_p ~perm:0o777 out_dir;
     
-  Core.Out_channel.write_all (Printf.sprintf "%s/index.html" out_dir) ~data:(Soup.to_string index_html);
-  prerr_endline (Printf.sprintf "Site generated at: '%s'" out_dir)
+  Soup.write_file (sprintf "%s/index.html" out_dir) (Soup.to_string index_page);
+  prerr_endline (sprintf "Site generated at: '%s'" out_dir)
 
 let () = 
   let current_dir = Core.Sys.getenv_exn "PROJECT_ROOT"  in
-  let index_path = Printf.sprintf "%s/content/pages/index.md" current_dir in
-  let index_html = (generate_html_from_markdown ~input_file_path:index_path) |> Soup.parse in
-  let template_html = Soup.read_file (Printf.sprintf "%s/content/templates/template.html" current_dir) 
-    |> Soup.parse in
-  Soup.replace (Soup.($) template_html "#page-content") index_html;
-  build_site ~out_dir:"dist" ~index_html:template_html
+  (* Generate index page *)
+  let index_page = generate_index_page current_dir in
+  (* Generate index page *)
+  build_directory ~out_dir:"dist" ~index_page:index_page
