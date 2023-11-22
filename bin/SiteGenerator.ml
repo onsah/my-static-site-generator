e80515b8719641b3594ad3cd5aad506e28c2a25c
Onur Şahin <onur@carbonhealth.com>
2023-11-22 21:36:39 +0100

    refactor

--- bin/SiteGenerator.ml
+++ bin/SiteGenerator.ml
@@ -1,4 +1,7 @@
 open Core
+
+let ( $ ) = Soup.( $ )
+
 open Site
 
 let generate_html_from_markdown ~input_file_path =
@@ -6,15 +9,27 @@ let generate_html_from_markdown ~input_file_path =
   let markdown = Omd.of_string file_str in
   Omd.to_html markdown
 
+let instantiate_template_for_index_page (template : Site.page)
+    (content : Site.page) =
+  Soup.replace (template $ "#page-content") content
+
+let index_page_template_path =
+  Filename.concat Environment.project_root
+    (Filename.of_parts [ "content"; "templates"; "template.html" ])
+
 let generate_index_page () =
   let index_page_path = Filename.of_parts [ "content"; "pages"; "index.md" ] in
   let index_page_abs_path =
     Filename.concat Environment.project_root index_page_path
   in
-  let index_html =
+  let index_page_content =
     generate_html_from_markdown ~input_file_path:index_page_abs_path
     |> Soup.parse
   in
-  Template.apply_for_index_page ~content:index_html
+  let index_page =
+    index_page_template_path |> Core.In_channel.read_all |> Soup.parse
+  in
+  instantiate_template_for_index_page index_page index_page_content;
+  index_page
 
 let generate () = { index_page = generate_index_page () }
