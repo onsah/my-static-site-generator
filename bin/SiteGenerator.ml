47dc8c96396566412e2e78bee654f435dd6cfe79
Onur Şahin <onur@carbonhealth.com>
2023-11-24 10:55:48 +0100

    feat: header template

--- bin/SiteGenerator.ml
+++ bin/SiteGenerator.ml
@@ -12,9 +12,17 @@ let generate_html_from_markdown ~markdown_str =
   let markdown = Omd.of_string markdown_str in
   Omd.to_html markdown
 
-let instantiate_template_for_index_page (template : Site.page)
-    (content : Site.page) =
-  Soup.replace (template $ "#page-content") content
+let get_header_component (content_path : Filename.t) =
+  let path =
+    Filename.concat content_path
+      (Filename.of_parts [ "templates"; "header.html" ])
+  in
+  path |> In_channel.read_all |> Soup.parse
+
+let instantiate_template_for_index_page ~(template : Site.page)
+    ~(header : Site.page) ~(content : Site.page) =
+  Soup.replace (template $ "#page-content") content;
+  Soup.replace (template $ "#header") header
 
 let generate_index_page (content_path : Filename.t) =
   let index_content_path =
@@ -30,7 +38,9 @@ let generate_index_page (content_path : Filename.t) =
       (Filename.of_parts [ "templates"; "index.html" ])
   in
   let index_page = index_page_path |> Core.In_channel.read_all |> Soup.parse in
-  instantiate_template_for_index_page index_page index_content_page;
+  let header_component = get_header_component content_path in
+  instantiate_template_for_index_page ~template:index_page
+    ~header:header_component ~content:index_content_page;
   index_page
 
 let generate site_generator =
