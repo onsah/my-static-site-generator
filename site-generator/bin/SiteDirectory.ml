b1a2a7223f59c68821df995a253db85d2468f53e
Onur Sahin <sahinonur2000@hotmail.com>
2024-04-12 16:12:08 +0200

    chore: cleanup SiteDirectorty.create

--- site-generator/bin/SiteDirectory.ml
+++ site-generator/bin/SiteDirectory.ml
@@ -2,43 +2,7 @@ open Core
 
 let sprintf = Printf.sprintf
 
-type site_directory = { out_path : Filename.t }
 type site_directory2 = { out : Site.Path.t }
-
-let make ~out_path = { out_path }
-
-(* User: read write execute, rest: only read *)
-let unix_file_permissions = 0o744
-
-let create site_directory ~(site : Site.t) =
-  let out_path = site_directory.out_path in
-  (match (Sys_unix.file_exists out_path, Sys_unix.is_directory out_path) with
-  | `No, _ ->
-      print_endline
-        (sprintf "Out directory %s does not exit. Creating..." out_path);
-      Core_unix.mkdir_p ~perm:unix_file_permissions out_path
-  | _, `No ->
-      print_endline
-        (sprintf "Out directory %s already exists and it's not a folder."
-           out_path);
-      exit 1
-  | _ -> ());
-  Out_channel.write_all
-    (sprintf "%s/index.html" out_path)
-    ~data:(Soup.to_string site.index_page);
-  Out_channel.write_all
-    (Filename.concat out_path "blog.html")
-    ~data:(Soup.to_string site.blog_page);
-  Core_unix.mkdir_p ~perm:unix_file_permissions
-    (Filename.concat out_path "posts");
-  ignore
-    (List.map site.posts ~f:(fun post ->
-         printf "out path: %s\n" (Filename.concat out_path post.path);
-         Out_channel.write_all
-           (Filename.concat out_path post.path)
-           ~data:(Soup.to_string post.page);
-         ()));
-  prerr_endline (sprintf "Site generated at: '%s'" out_path)
 
 let create_directory_if_not_exists (path : Site.Path.t) =
   let path_str = Site.Path.to_string path in
--- site-generator/bin/SiteDirectory.mli
+++ site-generator/bin/SiteDirectory.mli
@@ -1,11 +1,8 @@
-open Core
+open! Core
 (** 
     Module for creating the static site directory    
 *)
 
-type site_directory
 type site_directory2 = { out : Site.Path.t }
 
-val make : out_path:Filename.t -> site_directory
-val create : site_directory -> site:Site.t -> unit
 val create2 : site_directory2 -> site:Site.t2 -> unit
