613f5634505553663b451f0dd3fe841e7b2e6b96
Chukwuma Akunyili <chukwumaakunyili@gmail.com>
2025-05-19 08:11:13 +0000

    Refactor code for improved readability and consistency
    - Simplified flag definitions in `main.ml` for better clarity.
    - Enhanced formatting and alignment in `MySequence.ml` for consistency.
    - Streamlined pattern matching and error handling in `NonEmptyStack.ml`.
    - Improved structure and readability in `TemplatingEngine.ml` and its interface.
    - Adjusted comments and documentation for clarity and completeness.

--- site-generator/lib/MySequence.ml
+++ site-generator/lib/MySequence.ml
@@ -10,29 +10,30 @@ let of_iterator (type a) (iter : a iterator) : a t =
     type _ Effect.t += Yield : a -> unit Effect.t
   end in
   let yield x = perform (Yield x) in
-  match_with
-    iter
-    yield
-    { retc = (fun _ -> empty)
-    ; effc =
+  match_with iter yield
+    {
+      retc = (fun _ -> empty);
+      effc =
         (* TODO: Use dedicated effect syntax when we have OCaml 5.3 *)
         (fun (type c) (eff : c Effect.t) ->
           match eff with
           | Yield x ->
-            Some (fun (k : (c, _) continuation) -> append (return x) (continue k ()))
-          | _ -> None)
-    ; exnc = raise
+              Some
+                (fun (k : (c, _) continuation) ->
+                  append (return x) (continue k ()))
+          | _ -> None);
+      exnc = raise;
     }
-;;
 
-type ('a, 'b) fallible_iter_args =
-  { yield : 'a -> unit
-  ; abort : 'c. 'b -> 'c
-  }
+type ('a, 'b) fallible_iter_args = {
+  yield : 'a -> unit;
+  abort : 'c. 'b -> 'c;
+}
 
 type ('a, 'b) fallibe_iterator = ('a, 'b) fallible_iter_args -> unit
 
-let of_fallible_iterator (type a b) (iter : (a, b) fallibe_iterator) : (a t, b) result =
+let of_fallible_iterator (type a b) (iter : (a, b) fallibe_iterator) :
+    (a t, b) result =
   let open Effect in
   let open Effect.Deep in
   let open struct
@@ -45,21 +46,21 @@ let of_fallible_iterator (type a b) (iter : (a, b) fallibe_iterator) : (a t, b) 
   match_with
     (fun () -> iter { yield; abort })
     ()
-    { retc = (fun _ -> Ok empty)
-    ; effc =
+    {
+      retc = (fun _ -> Ok empty);
+      effc =
         (* TODO: Use dedicated effect syntax when we have OCaml 5.3 *)
         (fun (type c) (eff : c Effect.t) ->
           match eff with
           | Yield x ->
-            Some
-              (fun (k : (c, _) continuation) ->
-                let open Core.Result.Let_syntax in
-                let%map rest = continue k () in
-                append (Sequence.return x) rest)
-          | _ -> None)
-    ; exnc =
+              Some
+                (fun (k : (c, _) continuation) ->
+                  let open Core.Result.Let_syntax in
+                  let%map rest = continue k () in
+                  append (Sequence.return x) rest)
+          | _ -> None);
+      exnc =
         (function
-          | Abort error -> Error error
-          | exn -> raise exn)
+        | Abort error -> Error error
+        | exn -> raise exn);
     }
-;;
